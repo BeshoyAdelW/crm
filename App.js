ebe766962727185b131fb481b9172be4ea401a52
Beshoy Adel <Beshoy.AdelWaheeb@yahoo.com>
2021-02-03 15:02:12 +0200

    Integrated react native debugger

--- App.js
+++ App.js
@@ -5,13 +5,16 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./src/reducers/PeopleReducer";
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>Welcome to our CRM</Text>
+        <Text>Welcome to our CRM!</Text>
         <StatusBar style="auto" />
       </View>
     </Provider>
