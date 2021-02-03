47c882e59321a1618c6160f3d9597ebffe1bab23
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2021-02-03 20:22:51 +0200

    Styled people item components

--- src/components/PeopleItem.js
+++ src/components/PeopleItem.js
@@ -1,30 +1,52 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { connect } from "react-redux";
 import * as actions from "../actions";
+import { getTheme } from "react-native-material-kit";
+import { EvilIcons } from "@expo/vector-icons";
 
 function PeopleItem({ people }) {
+  const theme = getTheme();
   return (
-    <View style={styles.card}>
-      <Text>
+    <View style={[theme.cardStyle, styles.card]}>
+      <Image
+        source={require("../../assets/background.jpg")}
+        style={[theme.cardImageStyle, styles.image]}
+      />
+      <EvilIcons name="user" size={100} style={styles.icon} />
+      <Text style={[theme.cardTitleStyle, styles.title]}>
         {people.firstName} {people.lastName}
       </Text>
-      <Text>{people.phone}</Text>
+      <Text style={[theme.cardActionStyle, styles.action]}>
+        {people.company}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    margin: 4,
-    borderWidth: 1,
-    padding: 12,
-    borderRadius: 6,
+    marginTop: 20,
+  },
+  title: {
+    top: 20,
+    left: 80,
+    fontSize: 24,
+  },
+  image: {
+    height: 100,
+  },
+  action: {
+    backgroundColor: "black",
+    color: "white",
+  },
+  icon: {
+    position: "absolute",
+    top: 15,
+    left: 0,
+    color: "white",
+    backgroundColor: "rgba(255,255,255,0)",
   },
 });
-
-const mapStateToProps = (state) => {
-  return { people: state.people };
-};
 
 export default connect(null, actions)(PeopleItem);
