6bda545235c347146634e9205d9f1e7103b10363
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2021-02-06 13:38:15 +0200

    Linked the people detail view to the people list

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,25 +1,34 @@
 import React from "react";
+import Constants from "expo-constants";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import { connect } from "react-redux";
 import colors from "../config/colors";
+import PeopleDetail from "./PeopleDetail";
 import PeopleItem from "./PeopleItem";
 
-function PeopleList({ people }) {
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={people}
-        renderItem={({ item }) => <PeopleItem people={item} />}
-        keyExtractor={(item) => {
-          return item.id.toString();
-        }}
-      />
-    </View>
-  );
+function PeopleList({ people, detailView }) {
+  const renderInitialView = () => {
+    if (detailView == true) {
+      return <PeopleDetail />;
+    } else {
+      return (
+        <FlatList
+          data={people}
+          renderItem={({ item }) => <PeopleItem people={item} />}
+          keyExtractor={(item) => {
+            return item.id.toString();
+          }}
+        />
+      );
+    }
+  };
+
+  return <View style={styles.container}>{renderInitialView()}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: colors.white,
     alignItems: "center",
@@ -28,7 +37,7 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = (state) => {
-  return { people: state.people };
+  return { people: state.people, detailView: state.detailView };
 };
 
 export default connect(mapStateToProps)(PeopleList);
